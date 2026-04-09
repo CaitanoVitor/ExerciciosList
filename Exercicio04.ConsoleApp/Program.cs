5b2b90804b783dd2d945475b3374774b37095599
Vitor Caitano <caitanoovitor@gmail.com>
2026-04-09 12:13:18 -0300

    Exercicio 04 concluido

---
+++ Exercicio04.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Calculadora de Salário de Vendedores");
+
+Console.Write("Digite o salário base: ");
+
+double salarioBase = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o total de vendas: ");
+
+double totalVendas = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o percentual de comissão (%): ");
+
+double percentualComissao = Convert.ToDouble(Console.ReadLine());
+
+double salarioTotal = salarioBase + (totalVendas * percentualComissao / 100);
+
+Console.WriteLine($"O salário total é: R$ {salarioTotal:F2}");
+
+Console.ReadLine();
