ec850671c0b51315476922ffa5cb251c2b5c23fc
Vitor Caitano <caitanoovitor@gmail.com>
2026-04-09 15:24:29 -0300

    Exercicio 08 concluido

---
+++ Exercicio08.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Digite um número: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+Console.WriteLine("O número é par.");
+}
+else
+{
+Console.WriteLine("O número é ímpar.");
+}
+Console.ReadLine();
