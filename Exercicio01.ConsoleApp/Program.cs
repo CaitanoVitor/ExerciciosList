b7f886352604acdd86db94c2069ca914886d9b30
Vitor Caitano <caitanoovitor@gmail.com>
2026-04-09 11:12:06 -0300

    Adcionado o exercicio 01

--- Exercicio01.ConsoleApp/Program.cs
+++ Exercicio01.ConsoleApp/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Digite o COMPRIMENTO da caixa RETANGULAR: ");
+double comprimento = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o LARGURA da caixa RETANGULAR: ");
+double largura = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o ALTURA da caixa RETANGULAR: ");
+double altura = Convert.ToDouble(Console.ReadLine());
+
+double volume = comprimento * largura * altura;
+
+Console.WriteLine($"O Volume da CAIXA é: {volume:F2} cm³");
+
+Console.ReadLine();
