53d47b634029cfa47f1ec4a8cab01c8472b53a29
Vitor Caitano <caitanoovitor@gmail.com>
2026-04-09 12:08:21 -0300

    Exercicio 03 concluido

---
+++ Exercicio03.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿decimal temperaturaCelsius, temperaturaFahrenheit;
+
+Console.WriteLine("Digite a temperatura em graus Celsius: ");
+temperaturaCelsius = Convert.ToDecimal(Console.ReadLine());
+
+temperaturaCelsius = (temperaturaCelsius * 9 /5) + 32;
+
+Console.WriteLine("A temperetura em Fahrenheit é: " + temperaturaCelsius);
+
+Console.ReadLine();
