7e0abdfc0c7a5c2bef55e62433ebf5e95b353e1a
Vitor Caitano <caitanoovitor@gmail.com>
2026-04-09 11:32:09 -0300

    Exercicio 02 concluido

---
+++ Exercicio02.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Qual é o KM até o inicial do veiculo: ");
+double kmInicial = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Qual é o KM até o Final do veiculo: ");
+double kmFinal = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o consumo do combustivel em Litros após a viagem: ");
+double consumoCombustivel = Convert.ToDouble(Console.ReadLine());
+
+
+double distanciaPercorrida = kmFinal - kmInicial;
+
+double consumoKm = distanciaPercorrida / consumoCombustivel;
+
+Console.WriteLine($"O consumo do combustivel durante a viagem foi de ~ {consumoKm:F2} KM/L");
+
+Console.ReadLine();
